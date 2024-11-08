c8ae4a9488d1d510a407a0b68a4d22702482f495
SuelenCbrl <suelen.cbrl.sc@gmail.com>
2024-11-08 11:44:39 -0400

    tentativa01
    tenativa 01 de listar registros do banco de dados com PHP e PDO

---
+++ areaPrivada.php
@@ -0,0 +1,22 @@
+<!-- <?php
+    // Class teste{
+    //     try{
+    //         $sql="SELECT * FROM usuarios";
+    //         $sonsulta=$pdo->query($sql);
+    //         if($consulta->rowCount()>0)
+    //             while ($row = $consulta->fetch(PDO::FETCH_COLUMN)) {
+    //             echo "ID: " . $row['id'] . "<br>";
+    //             echo "Nome: " . $row['nome'] . "<br>";
+    //             echo "Email: " . $row['email'] . "<br>";
+    //             echo "<hr>";
+    //         }
+    //     }   
+    //         else {
+    //             echo "Nenhum registro encontrado.";
+    //     }
+    //     catch (PDOException $erro){
+    //         $msgError = $erro->getMessage();
+    //     }
+    
+    // }
+?> -->
